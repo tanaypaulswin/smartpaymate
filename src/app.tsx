01c1cd721927b2fc218aa714e3ccf48c9fb7dee4
tanaypaulswin <tpaul@swin.edu.au>
2020-04-15 17:36:42 +1000

    copyright is added.

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+import "./app.scss";
+
 const StyledApp = styled('div')`
   background: #f9fbfb;
   height: 100vh;
@@ -11,6 +13,9 @@ const App = () => {
   return (
     <StyledApp>
       <h1>SmartPayMate - A new way to handle your bills and payments.</h1>
+      <div className="copyrightLabel">
+          © PSS Ltd. {new Date().getFullYear()}&nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
     </StyledApp>
   );
 };
